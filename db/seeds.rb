33c25e64bc78add81e9c5f68d08313249214efb4
Fikutaro <nomio9029@gmail.com>
2021-06-12 07:03:29 +0000

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tag.create!(
+  [
+    {
+      tag_name: 'かんたん'
+    },
+    {
+      tag_name: 'あそべる'
+    },
+    {
+      tag_name: 'たんじかん'
+    }]
+  )
